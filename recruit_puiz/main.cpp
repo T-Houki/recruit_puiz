956f48db1875ec2163423a1bd32230d01274603c
T-Houki <taka.tomo.0115@gmail.com>
2026-04-28 11:10:33 +0900

    円錐の体積を求める問題を追加

--- recruit_puiz/main.cpp
+++ recruit_puiz/main.cpp
@@ -41,8 +41,14 @@ int main()
 
 	//三角形の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
-	x = uniform_int_distribution<>(1, 5)(rand) * 2;
+	y = uniform_int_distribution<>(1, 5)(rand) * 3;
 	questions.push_back({ "面積" + to_string(x*y/2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ", x});
+
+	//円錐の体積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({ "低面積の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + 
+		"この円錐の体積をXπcm^3とする。Xの値を求めよ" , x * x * y / 3});
 
 	cout << "[リクルート試験対策クイズ]\n";
 
