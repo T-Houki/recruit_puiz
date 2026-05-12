e2e2dbe3aac0bc27157f565d41b6fea237018076
T-Houki <taka.tomo.0115@gmail.com>
2026-05-12 12:07:40 +0900

    意味から熟語を選ぶ処理

--- recruit_puiz/utility.h
+++ recruit_puiz/utility.h
@@ -1,5 +1,18 @@
 #pragma once
 #include <vector>
 
+//配列をシャッフルする
+void Shuffle(std::vector<int>& indices);
+
 //ランダムな番号配列を作成する
 std::vector<int> CreateRandomIndices(int n);
+
+//間違った番号の配列を作成する
+// ｎ			作成する番号の範囲
+// correctIndex 正解の番号
+
+/// <summary>/// 間違った番号の配列を作成する /// </summary>
+/// <param name="n">作成する番号の範囲</param>
+/// <param name="correctIndex">正解の番号</param>
+/// <returns></returns>
+std::vector<int> CreateWrongIndices(int n, int correctIndex);
